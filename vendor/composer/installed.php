5cbad57be58a1841bf04c9458c782fd4442130a2
Dinesh Kumar <mapmydestination@gmail.com>
2025-05-05 17:47:11 +0530

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'unopim/dam',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '51c4de6047db6f21da233b1207673a9796502369',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'unopim/dam' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '51c4de6047db6f21da233b1207673a9796502369',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
